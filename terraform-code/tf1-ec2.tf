211078da7cf4132839cb5ae97404cd331f02de20
biradarofficial <biradar.engineer@gmail.com>
2023-10-10 17:43:29 +0530

    v1 , v2 terraform files

---
+++ terraform-code/tf1-ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+     region = "us-east-1"
+     
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-067d1e60475437da2"
+  instance_type = "t2.micro"
+  key_name       = "shyam-practice"
+  
+ }
