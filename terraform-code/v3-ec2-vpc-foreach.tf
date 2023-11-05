2d9ee6a8ea3da1f1c921666a51a38292cec07cce
biradarofficial <145013159+biradarofficial@users.noreply.github.com>
2023-11-05 12:38:42 +0530

    Update v3-ec2-vpc-foreach.tf

--- terraform-code/v3-ec2-vpc-foreach.tf
+++ terraform-code/v3-ec2-vpc-foreach.tf
@@ -2,55 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-053b0d53c279acc90"
-    instance_type = "t2.micro"
-    key_name = "shyam-practice"
-    //security_groups = [ "demo-sg" ]
-    vpc_security_group_ids = [aws_security_group.demo-sg.id]
-    subnet_id = aws_subnet.dpp-public-subnet-01.id 
-    for_each = toset(["jenkins-master", "build-slave", "ansible"])
-   tags = {
-     Name = "${each.key}"
-   } 
-}
-
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSH Access"
-  vpc_id = aws_vpc.dpp-vpc.id 
-  
-  ingress {
-    description      = "Shh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-    ingress {
-    description      = "jenkins port"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "ssh-prot"
-
-  }
-}
-
 resource "aws_vpc" "dpp-vpc" {
   cidr_block = "10.1.0.0/16"
   tags = {
@@ -103,3 +54,52 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
   subnet_id = aws_subnet.dpp-public-subnet-02.id 
   route_table_id = aws_route_table.dpp-public-rt.id   
 }
+resource "aws_instance" "demo-server" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "shyam-practice"
+    //security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id 
+    for_each = toset(["jenkins-master", "build-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   } 
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id 
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description      = "jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
+}
+
